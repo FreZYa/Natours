40e88297932338fcec2975e75f3e0b797892c86b
FreZYa <ahmetresul159@gmail.com>
2025-02-23 17:08:37 +0300

    security controls added and user implements are done

--- models/userModel.js
+++ models/userModel.js
@@ -41,7 +41,12 @@ const userSchema = new mongoose.Schema({
     },
     passwordChangedAt: Date,
     passwordResetToken: String,
-    passwordResetExpires: Date
+    passwordResetExpires: Date,
+    active: {
+        type: Boolean,
+        default: true,
+        select: false
+    }
 });
 
 userSchema.pre('save', async function(next) {
@@ -56,7 +61,24 @@ userSchema.pre('save', async function(next) {
     next();
 });
 
-userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+userSchema.pre('save', function(next) {
+    // this points to the current document
+    if (!this.isModified('password') || this.isNew) return next();
+
+    this.passwordChangedAt = Date.now() - 1000;
+    next();
+});
+
+userSchema.pre(/^find/, function(next) {
+    // this points to the current query
+    this.find({active: {$ne: false}});
+    next();
+});
+
+userSchema.methods.correctPassword = async function(
+    candidatePassword, 
+    userPassword
+) {
     return await bcrypt.compare(candidatePassword, userPassword);
 }
 
