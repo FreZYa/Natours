cb93ae39f75c5be69fb8bb60c2344cb0999c7772
FreZYa <ahmetresul159@gmail.com>
2025-03-19 00:58:55 +0300

    Geospatial queries and aggregations implemented.

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -23,6 +23,12 @@ router
 router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
+router.route('/tours-within/:distance/center/:latlng/unit/:unit').get(tourController.getToursWithin)
+// /tours-distannce?distance=?233&center=-40,45&unit&mi
+// /tours-within/233/center/-40,45/unit/mi
+
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances)
+
 router
     .route('/')
     .get(tourController.getAllTours)
