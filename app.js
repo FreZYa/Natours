7bd92d38f8f390d738c23fa028c6906bbaa7ab9e
FreZYa <ahmetresul159@gmail.com>
2025-05-08 23:59:58 +0300

    views created and pug installed

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -10,10 +11,15 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
-
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1) GLOBAL MIDDLAWARE
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Set security HTTP headers
 app.use(helmet());
 
@@ -47,18 +53,19 @@ app.use(
     })
 );
 
-app.use(express.static(`${__dirname}/public`))
-
 // Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 });
 
+// 3) ROUTES
+app.get('/', (req, res) => {
+    res.status(200).render('base');
+})
+
 app.use('/api/v1/tours', tourRouter);
-
 app.use('/api/v1/users', userRouter);
-
 app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
