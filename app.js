a12da78b9fc923d097e4d532c30359d03d9a8f79
FreZYa <ahmetresul159@gmail.com>
2025-05-10 00:15:47 +0300

    Pug files prepared. Overview and tour routes implemented

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 const app = express();
+
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
@@ -60,10 +62,8 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.get('/', (req, res) => {
-    res.status(200).render('base');
-})
 
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
