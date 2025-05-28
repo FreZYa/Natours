c4edeb5be13a0d12d6d10d949602eaf0053439e6
FreZYa <ahmetresul159@gmail.com>
2025-05-28 22:24:20 +0300

    Implemented CORS

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.enable('trust proxy');
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-// 1) GLOBAL MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES 
 // Implement CORS
 app.use(cors());
 
